2ab17753bb8b62ee0bc273b512daa1f862f4a96d
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-06-16 13:49:45 +0200

    btn test

--- front/sw_catalogus/src/App.js
+++ front/sw_catalogus/src/App.js
@@ -1,16 +1,18 @@
-import { Button, Rating, Typography } from "@mui/material";
+import { Button, Rating } from "@mui/material";
+import { useState } from 'react';
 
 
 function App() {
+  const [name, setName] = useState("Test Naam");
 
   const btnClicked = (e) => {
-    e.value = "test";
+    setName(e);
   }
 
   return (
     <div className="App">
       
-      <Button variant="contained" onClick={btnClicked}>Test Button</Button>
+      <Button variant="contained" onClick={() => { btnClicked("Nieuwe Naam") }}>{name}</Button>
       <Rating name="simple-controlled" value={1 + 1}/>
     </div>
   );
