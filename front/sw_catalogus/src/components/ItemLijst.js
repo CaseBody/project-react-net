60672e4e685232a82fc0984083de1d6ccfa666f3
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-07-10 18:49:10 +0200

    add navigate to item

--- front/sw_catalogus/src/components/ItemLijst.js
+++ front/sw_catalogus/src/components/ItemLijst.js
@@ -1,11 +1,15 @@
 import { Box, Grid, Paper } from '@mui/material'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router-dom';
 import React from 'react'
+import {useNavigate} from 'react-router-dom';
 
 export const ItemLijst = (props) => {
+    const navigate = useNavigate();
+
     return(
         <Box sx={{ backgroundColor: "background.paper", width: '100%', minHeight:  'calc(100vh - 64px)' }}>
             <Box sx={{ display: 'flex', alignItems: 'baseline', flexWrap: 'wrap',width: '100%' }}>
-            { props.items.map( (item) => (<Paper key={item.id} sx={{ marginLeft: "25px", marginTop: "25px", height: '200px', width: '150px', backgroundImage: `url(${item.image})`, backgroundSize: 'cover', '&:hover': { cursor: "pointer", }}}></Paper>) )}
+            { props.items.map( (item) => (<Paper onClick={() => { navigate('/item') }} key={item.id} sx={{ marginLeft: "25px", marginTop: "25px", height: '200px', width: '150px', backgroundImage: `url(${item.image})`, backgroundSize: 'cover', '&:hover': { cursor: "pointer", }}}></Paper>) )}
             </Box>
         </Box>
     )
