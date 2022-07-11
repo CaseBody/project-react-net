35db8b853569e7a132dd815db50e5e8c3c27b08c
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-07-11 23:08:48 +0200

    project files back

---
+++ back/SWCatalogusAPI/SWCatalogusAPI/Controllers/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SWCatalogusAPI.Models;
+
+namespace SWCatalogusAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> Get()
+        {
+            var i = new List<Item>();
+            return Ok(i);
+        }
+    }
+}
---
+++ back/SWCatalogusAPI/SWCatalogusAPI/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace SWCatalogusAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; } = string.Empty;
+        public string Categorie { get; set; } = string.Empty;
+        public int UitgaveJaar { get; set; }
+    }
+}
